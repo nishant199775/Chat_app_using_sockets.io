eafbc57ce8cc1caf1e6f7cc5c74c38fd2a4f2eac
nishant <nkumar199775@gmail.com>
2020-04-23 11:48:35 +0530

    final commit

---
+++ public/script.js
@@ -0,0 +1,32 @@
+let socket=io()
+let login1=false
+$('#inp').show()
+$('#chatbox').hide()
+
+$('#start').click(function(){
+    
+    socket.emit("login",{unm:$('#inpbox').val(),
+        pwd:$('#pwd').val()
+})
+})
+$('#send').click(function(){
+    socket.emit("msg_send",{to:$('#sendto').val(),
+    msg:$('#msgbox').val()})
+    
+})
+socket.on('logged_in',function(){
+    login1=true
+    $('#inp').hide()
+    $('#chatbox').show()
+})
+socket.on('login_failed',function(){ 
+
+    window.alert("username or password is not correct")
+})
+
+
+socket.on('msg_received',function(data)
+{     
+    if(login1)
+    $('#ulmsg').append($('<li class="list-group-item">').text("["+data.from+"]: "+"  "+data.msg))
+})
---
+++ server.js
@@ -0,0 +1,52 @@
+const http=require('http')
+const express=require('express')
+const app=express()
+const server=http.createServer(app)
+const socket=require("socket.io")
+const io=socket(server)
+let users={
+  nishant:1234
+}
+let socketmap={}
+io.on("connection",function(socket){
+  console.log("connected with id="+socket.id)
+  socket.on('login',function(data){
+    if(users[data.unm])
+    {
+      if(data.pwd==users[data.unm])
+      {
+        socket.join(data.unm)
+        socket.emit('logged_in')
+        socketmap[socket.id]=data.unm
+      }
+      else{
+        socket.emit('login_failed')
+      
+      }
+    }
+    else{
+      socket.join(data.unm)
+      users[data.unm]=data.pwd
+      socket.emit('logged_in')
+      socketmap[socket.id]=data.unm
+    }
+    
+  })
+  socket.on('msg_send',function(data){
+    if(users[data.to]){
+
+      io.to(data.to).emit('msg_received', {msg:data.msg,from:socketmap[socket.id]})
+    }
+    else{
+      socket.broadcast.emit('msg_received', {msg:data.msg,from:socketmap[socket.id]})
+    }
+    
+    
+  })
+})
+
+app.use('/', express.static(__dirname + '/public'))
+
+server.listen(3344, function() {
+  console.log('Started on http://localhost:3344')
+})
